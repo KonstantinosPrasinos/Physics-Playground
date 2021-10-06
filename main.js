fbd8355ae38db72444ce43dea4cb46726e421504
KonstantinosPrasinos <konstantinos.prasinos@gmail.com>
2021-10-06 20:24:00 +0300

    Add selection buttons to top ui Add play and change mode to top ui Recreate simulation object Add basic Physics Add play/pause

--- main.js
+++ main.js
@@ -1,220 +1,164 @@
 let canvas = document.getElementById("viewportCanvas");
 
-const scene = new THREE.Scene();
+let scene, renderer, camera, world, timeStep = 1 / 60;
 
-const renderer = new THREE.WebGLRenderer({ canvas: viewportCanvas, antialias: true});
-renderer.setClearColor( 0xffffff, 1);
+let savedSimulationState = {};
 
-renderer.setSize(parseInt(window.getComputedStyle(canvas).width), parseInt(window.getComputedStyle(canvas).height));
-
-let cameraMetrics = {
-    viewHeight: 900,
-    aspectRatio: parseInt(window.getComputedStyle(canvas).width) / parseInt(window.getComputedStyle(canvas).height),
-    near: 0.1,
-    far: 20000,
-    viewAngle: 45
+function isObject(item){
+    return (typeof item === "object" && !Array.isArray(item) && item !== null);
 }
 
-let camera = new THREE.PerspectiveCamera( cameraMetrics.viewAngle, cameraMetrics.aspectRatio, cameraMetrics.near, cameraMetrics.far);
-// var winResize	= new THREEx.WindowResize(renderer, camera);
-camera.position.set(0,150,400);
-camera.lookAt(scene.position);
+function initThree() {
+    scene = new THREE.Scene();
 
-// const camera = new THREE.OrthographicCamera(-cameraMetrics.aspectRatio * cameraMetrics.viewHeight / 2, cameraMetrics.aspectRatio * cameraMetrics.viewHeight / 2, cameraMetrics.viewHeight / 2, -cameraMetrics.viewHeight / 2, -1000, 1000);
+    camera = new THREE.PerspectiveCamera( 45, parseInt(window.getComputedStyle(canvas).width) / parseInt(window.getComputedStyle(canvas).height), 1, 2000);
+    camera.position.z = 100;
+    scene.add(camera);
 
-let simulation = {
-    gravity: 10,
-    time: 0, //in ms
-    timePerTick: 1, //in ms
-    items: [],
-    numberOfObjects: {
-        boxes: []
-    },
-    tickHandling: function () {
-        setInterval(() => {
-            this.items.forEach(item => {
-                item.checkDimensions();
-                item.moveX();
-                item.moveY();
-            })
-            this.time += this.timePerTick;
-        }, this.timePerTick);
-    },
-    deleteObject: function (index) {
-        this.items[index].removeFromScene();
-        this.items.splice(index, 1);
-        this.refreshListOfObjects();
-    },
-    addObjectToList(index){
-        let node = document.createElement("DIV");
-        let textNode = document.createElement("input");
-        let editButtonNode = document.createElement('input');
-        let deleteButtonNode = document.createElement('input');
-        let lockButtonNode = document.createElement('input');
-        
-        node.classList.add("item-list-field");
-        textNode.type = 'text';
-        textNode.value = this.items[index].name;
-        textNode.setAttribute('required', "");
-        textNode.classList.add("item-list-editable");
+    renderer = new THREE.WebGLRenderer({ canvas: viewportCanvas, antialias: true});
+    renderer.setClearColor( 0xffffff, 1);
+    renderer.setSize(parseInt(window.getComputedStyle(canvas).width), parseInt(window.getComputedStyle(canvas).height));
+}
 
-        editButtonNode.type = 'button';
-        editButtonNode.classList.add("icon-buttons");
-        editButtonNode.classList.add("item-list-field-edit-button");
-        editButtonNode.classList.add("small-icon-buttons");
-        editButtonNode.addEventListener('click', () =>{
-            textNode.focus();
-        });
+function initCannon() {
+    world = new CANNON.World();
+    world.gravity.set(0, 0, 0);
+    world.broadphase = new CANNON.NaiveBroadphase();
+    world.solver.iterations = 10;
+}
 
-        deleteButtonNode.type = 'button';
-        deleteButtonNode.classList.add("item-list-field-delete-button");
-        deleteButtonNode.classList.add("icon-buttons");
-        deleteButtonNode.classList.add("small-icon-buttons");
-        deleteButtonNode.addEventListener('click', () => {
-            this.deleteObject(index);
-        });
+function updatePhysics() {
+    world.step(timeStep);
 
+    simulation.boxes.forEach(element => {
+        element.mesh.position.copy(element.body.position);
+        element.mesh.quaternion.copy(element.body.quaternion);
+    });
+}
 
-        lockButtonNode.type = 'button';
-        lockButtonNode.classList.add("item-list-lock-button");
-        lockButtonNode.classList.add("icon-buttons");
-        lockButtonNode.classList.add("small-icon-buttons");
-        lockButtonNode.addEventListener('click', () => {
-            this.items[index].moveable = !this.items[index].moveable;
-            console.log(this.items[index].moveable);
-            if(!this.items[index].moveable) {
-                lockButtonNode.style.backgroundColor = 'orange';
-            } else {
-                lockButtonNode.style.backgroundColor = 'var(--secondary-color)';
-            }
-        })
+function render() {
+    renderer.render(scene, camera);
+}
 
-        textNode.addEventListener("blur", () => {
-            if (textNode.value.length == 0) {
-                textNode.focus();
-            } else {
-                this.items[index].name = textNode.value;
-            }
-        });
-        node.appendChild(textNode);
-        node.appendChild(deleteButtonNode);
-        node.appendChild(editButtonNode);
-        node.appendChild(lockButtonNode);
-        document.getElementById("right-ui-item-container").appendChild(node);
-    },
-    refreshListOfObjects(){
-        while(document.getElementById("right-ui-item-container").firstChild){
-            document.getElementById("right-ui-item-container").removeChild(document.getElementById("right-ui-item-container").firstChild);
-        }
-        for (let index in this.items){
-            this.addObjectToList(index);
-        }
-    },
-    addCube(coordX, coordY, coordz){
-        simulation.items.push(new item('box', { width: 10, height: 10, depth: 10 }, 0x000000, { x: coordX, y: coordY, z: coordz }));
-        this.refreshListOfObjects();
-    },
-    checkCollisions(){
-        let collisions = [];
-        for (const index in this.items){
-            for (const key in this.items) {
-                if (key == 0 && index == 1) {
-                    if (((this.items[index].position.x - (this.items[index].dimensions.width / 2)) <= (this.items[key].position.x + (this.items[key].dimensions.width) / 2)) && ((this.items[index].position.x + (this.items[index].dimensions.width / 2)) >= (this.items[key].position.x + (this.items[key].dimensions.width) / 2))){
-                        console.log("Type 1 collision");
+function animate() {
+    requestAnimationFrame(animate);
+    if (!simulation.isPaused){
+        updatePhysics();
+    }
+    render();
+}
+
+function makeCopyofBodies(){
+    for (let i = 0; i < simulation.boxes.length; i++){
+        // let tempBody = new CANNON.Body({
+        //     mass: simulation.boxes[i].mass
+        // });
+        // tempBody.shapes.push(simulation.boxes[i].body.shapes[0]);
+        // tempBody.position.set(simulation.boxes[i].body.position.clone());
+        // tempBody.velocity.set(simulation.boxes[i].body.velocity.clone());
+        // tempBody.force.set(simulation.boxes[i].body.force.clone());
+        // tempBody.quaternion.set(simulation.boxes[i].body.quaternion.clone());
+        // console.log(tempBody);
+        let clone = {};
+        for (key in simulation.boxes[i].body){
+            // console.log(simulation.boxes[i].body);
+            if (simulation.boxes[i].body){
+                if (isObject(simulation.boxes[i].body[key])){
+                    if (key === "world"){
+                        clone[key] = world;
+                    } else if (key === "invInertiaWorld"){
+                        clone[key] = simulation.boxes[i].body[key];
+                    } else if (key === "invInertiaWorldSolve"){
+                        clone[key] = simulation.boxes[i].body[key];
+                    } else {
+                        clone[key] = simulation.boxes[i].body[key].clone();
                     }
-                    if (((this.items[index].position.x - (this.items[index].dimensions.width / 2)) <= (this.items[key].position.x - (this.items[key].dimensions.width) / 2)) && ((this.items[index].position.x + (this.items[index].dimensions.width / 2)) >= (this.items[key].position.x - (this.items[key].dimensions.width) / 2))){
-                        console.log("Type 2 collision");
-                    }
+                } else {
+                    clone[key] = simulation.boxes[i].body[key];
                 }
             }
         }
     }
 }
 
-class item {
-    constructor(type, dimensions, color, position) {
-        this.type = type;
-        this.moveable = true;
-        this.itemNode;
+let simulation = {
+    boxes: [],
+    isPaused: true,
+    createBox(x, y, z, width, height, depth){
+        let shape = new CANNON.Box(new CANNON.Vec3(width / 2, height / 2, depth / 2));
+        let tempBody = new CANNON.Body({
+            mass: 1
+        });
+        tempBody.addShape(shape);
+        tempBody.position.set(x, y, z);
+        // tempBody.angularVelocity.set(0, 10, 0);
+        // tempBody.angularDamping = 0.5;
+        world.add(tempBody);
 
-        if (type == 'box') {
-            simulation.numberOfObjects.boxes++;
-            this.name = `Box${simulation.numberOfObjects.boxes}`;
-            this.geometry = new THREE.BoxGeometry(dimensions.width, dimensions.height, dimensions.depth);
-            this.material = new THREE.MeshBasicMaterial();
-            this.material.color.set(color);
-            this.object = new THREE.Mesh(this.geometry, this.material);
-            this.dimensions = dimensions;
-            this.position = position;
-            scene.add(this.object);
+        let geometry = new THREE.BoxGeometry( width, height, depth);
+        let material = new THREE.MeshBasicMaterial( {color: 0xff0000, wireframe: true} );
+        let tempMesh = new THREE.Mesh(geometry, material);
+        tempMesh.position.set(x, y, z);
+        scene.add(tempMesh);
+
+        let i = 0;
+        if (this.boxes.length){
+            for (i; i < this.boxes.length; ) {
+                for (const box of this.boxes) {
+                    if (box.name.includes('-')){
+                        let tempString = box.name.split('-');
+                        if (tempString[0] == 'Box' && (parseInt(tempString[1]).toString().length == tempString[1].length)){
+                            let number = parseInt(tempString[1]);
+                            if (number == i){
+                                i++;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
         }
-
-        this.velocity = {
-            x: 0,
-            y: 0,
-            z: 0
+        let box = {
+            body: tempBody,
+            mesh: tempMesh,
+            name: `Box-${i}`
         }
-
-        this.acceleration = {
-            x: 0,
-            y: 0,
-            z: 0
+        this.boxes.push(box);
+    },
+    checkForObject(event){
+        let mouseVector = new THREE.Vector2();
+        let rayCaster = new THREE.Raycaster();
+    
+        mouseVector.x = (event.offsetX /  parseInt(window.getComputedStyle(canvas).width)) * 2 - 1;
+        mouseVector.y = -(event.offsetY /  parseInt(window.getComputedStyle(canvas).height)) * 2 + 1;
+    
+        rayCaster.setFromCamera(mouseVector, camera);
+        const intersectedObjects = rayCaster.intersectObjects(scene.children);
+    
+        return intersectedObjects;
+    },
+    removeAllObjects(){
+        //Remove all Meshes from scene
+        for (let i = 0; i < scene.children.length; i++){
+            if(scene.children[i].type === "Mesh"){
+                scene.remove(scene.children[i]);
+            }
         }
-
-        this.removeFromScene = function () {
-            scene.remove(this.object);
+        //Remove all Bodies from world
+        for (let i = 0; i < world.bodies.length; i++){
+            world.remove(world.bodies[i]);
         }
-
-        this.moveX = function () {
-            this.position.x += (this.velocity.x * simulation.timePerTick) + (0.5 * this.acceleration.x * Math.pow(simulation.timePerTick / 1000, 2));
-            this.velocity.x += this.acceleration.x * simulation.timePerTick;
-            this.object.position.x = this.position.x;
-        }
-
-        this.moveY = function () {
-            this.position.y += (this.velocity.y * simulation.timePerTick) + (0.5 * this.acceleration.y * Math.pow(simulation.timePerTick / 1000, 2));
-            this.velocity.y += this.acceleration.y * simulation.timePerTick;
-            this.object.position.y = this.position.y;
-        }
-
-        this.moveZ = function () {
-            this.position.z += (this.velocity.z * simulation.timePerTick) + (0.5 * this.acceleration.z * Math.pow(simulation.timePerTick / 1000, 2));
-            this.velocity.z += this.acceleration.z * simulation.timePerTick;
-            this.object.position.z = this.position.z;
-        }
-
-        this.checkDimensions = function () {
-            this.dimensions.width = this.geometry.parameters.width * this.object.scale.x;
-            this.dimensions.height = this.geometry.parameters.height * this.object.scale.y;
-            this.dimensions.depth = this.geometry.parameters.depth * this.object.scale.z;
+        console.log("Removed All Objects");
+    },
+    addAllObjects(){
+        //Adds all Bodies to the world and Meshes to the scene
+        for (let i = 0; i < this.boxes.length; i++){
+            scene.add(this.boxes[i].mesh);
+            world.add(this.boxes[i].body);
         }
     }
 }
 
-// simulation.items.push(new item('box', { width: 10, height: 300, depth: 100 }, 0x000000, { x: 500, y: 100, z: 0 }));
-// simulation.items.push(new item('box', { width: 100, height: 300, depth: 100 }, 0x000000, { x: -670, y: 0, z: 0 }));
-// simulation.addCube();
-const animate = function () {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-};
-
+initThree();
+initCannon();
 animate();
-
-simulation.tickHandling();
-simulation.refreshListOfObjects();
-
-// var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 5000);
-
-function checkForObject(event){
-    let mouseVector = new THREE.Vector2();
-    let rayCaster = new THREE.Raycaster();
-
-    mouseVector.x = (event.offsetX /  parseInt(window.getComputedStyle(canvas).width)) * 2 - 1;
-    mouseVector.y = -(event.offsetY /  parseInt(window.getComputedStyle(canvas).height)) * 2 + 1;
-
-    rayCaster.setFromCamera(mouseVector, camera);
-    const intersectedObjects = rayCaster.intersectObjects(scene.children);
-
-    return intersectedObjects;
-}
--- styling.js
+++ styling.js
@@ -2,6 +2,8 @@ let rightUIisCollapsed = true;
 let storedTheme = 'dark';
 let itemSelected = -1;
 let tutorialCompleted = false;
+let mode = "setup";
+let setupHistory;
 // let colorPicker 
 
 let root = document.documentElement;
@@ -11,7 +13,6 @@ let collapseRightUIButton = document.getElementById("collapse-right-ui-button");
 let rightFeatures = document.getElementById("right-ui-features");
 let objectNameField = document.getElementById("object-name");
 let rightItems = document.getElementById("right-ui-items-list");
-let itemsInScene = document.getElementById("items-in-scene");
 let settingsOverlay = document.getElementById("settings-overlay");
 let settingsBox = document.getElementById("settings-box");
 let widthInput = document.getElementById("right-ui-width");
@@ -20,7 +21,12 @@ let depthInput = document.getElementById("right-ui-depth");
 let xInput = document.getElementById("right-ui-x");
 let yInput = document.getElementById("right-ui-y");
 let zInput = document.getElementById("right-ui-z");
+let rotationXInput = document.getElementById("right-rotation-x");
+let rotationYInput = document.getElementById("right-rotation-y");
+let rotationZInput = document.getElementById("right-rotation-z");
 let colorPicker = document.getElementById("color-picker");
+let togglePauseButton = document.getElementById("top-play");
+let topMode = document.getElementById("top-mode");
 
 if (!localStorage.theme) {
     localStorage.setItem("theme", "dark");
@@ -42,14 +48,13 @@ function toggleRightUI() {
         timeline.to(rightUI, { duration: 0.2, width: '180px' })
             .to(rightFeatures, { duration: 0.2, opacity: 1 })
             .to(rightItems, { duration: 0.2, opacity: 1 }, '-=0.2')
-            .to(itemsInScene, { duration: 0.2, opacity: 1 }, '-=0.2');
+            .to(objectNameField, {duration: 0.2, opacity: 1}, '-=0.2');
         rightUIisCollapsed = !rightUIisCollapsed;
 
     } else {
         let timeline = gsap.timeline();
         timeline.to(rightFeatures, { duration: 0.2, opacity: 0 })
             .to(rightItems, { duration: 0.2, opacity: 0 }, '-=0.2')
-            .to(itemsInScene, { duration: 0.2, opacity: 0 }, '-=0.2')
             .to(rightUI, { duration: 0.2, width: '0px' })
             .to(collapseRightUIButton, { duration: 0.2, right: '8px' }, '-=0.2')
             .to(collapseRightUIButton, { duration: 0.2, rotation: 180, onComplete: function () { rightUI.style.visibility = 'hidden'; } }, '-=0.2');
@@ -102,21 +107,67 @@ function setTheme(theme) {
 }
 
 function setInputObjectParameters() {
-    if (itemSelected >= 0) {
-        widthInput.value = simulation.items[itemSelected].dimensions.width;
-        heightInput.value = simulation.items[itemSelected].dimensions.height;
-        depthInput.value = simulation.items[itemSelected].dimensions.depth;
-        xInput.value = simulation.items[itemSelected].position.x;
-        yInput.value = simulation.items[itemSelected].position.y;
-        zInput.value = simulation.items[itemSelected].position.z;
-    } else {
-        widthInput.value = '';
-        heightInput.value = '';
-        depthInput.value = '';
-        xInput.value = '';
-        yInput.value = '';
-        zInput.value = '';
+    // if (itemSelected >= 0) {
+    //     widthInput.value = simulation.items[itemSelected].dimensions.width;
+    //     heightInput.value = simulation.items[itemSelected].dimensions.height;
+    //     depthInput.value = simulation.items[itemSelected].dimensions.depth;
+    //     xInput.value = simulation.items[itemSelected].position.x;
+    //     yInput.value = simulation.items[itemSelected].position.y;
+    //     zInput.value = simulation.items[itemSelected].position.z;
+    //     // rotationXInput.value = simulation.items[itemSelected].rotation.x;
+    // } else {
+    //     widthInput.value = '';
+    //     heightInput.value = '';
+    //     depthInput.value = '';
+    //     xInput.value = '';
+    //     yInput.value = '';
+    //     zInput.value = '';
+    // }
+}
+
+function pauseSimulation(){
+    if (!simulation.isPaused){
+        console.log("pausing simulation");
+        togglePauseButton.classList.remove('top-pause');
+        togglePauseButton.classList.add('top-play');
     }
+    simulation.isPaused = true;
+}
+
+function resumeSimulation(){
+    if (simulation.isPaused){
+        console.log("resuming simulation");
+        togglePauseButton.classList.remove('top-play');
+        togglePauseButton.classList.add('top-pause');
+        
+    }
+    simulation.isPaused = false;
+}
+
+function togglePause(){
+    if (mode == "setup"){
+        setupHistory = simulation;
+        mode = "simulation";
+        topMode.innerHTML = "<b>Mode:</b> Simulation";
+    }
+    if (simulation.isPaused){
+        resumeSimulation();
+    } else {
+        pauseSimulation();
+    }
+}
+
+function toggleMode(){
+    console.log(simulation.boxes[0].body);
+    // if (mode == "simulation"){
+    //     pauseSimulation();
+    //     mode = "setup";
+    //     simulation = setupHistory;
+    //     topMode.innerHTML = "<b>Mode:</b> Setup";
+    // }
+    //Make a new function for saving the state of the setup. Use the code below for the body. For the meshes use the whole save three.js save scene thing and export meshes from there. Don't forget to reset time to 0 (may have to do something else with time as well)
+    //I think that changing the mass of an object is harder than I would think. Google it (https://github.com/schteppe/cannon.js/issues/122).
+    //Instead of the thing I have below I can just copy everything to the new object except the things I have an issue with (the world child-object).
 }
 
 settingsOverlay.addEventListener('click', (event) => {
@@ -129,15 +180,14 @@ settingsOverlay.addEventListener('click', (event) => {
 
 canvas.addEventListener('mousedown', (event) => {
     if (document.activeElement !== colorPicker) {
-        let intersectedObjects = checkForObject(event);
+        let intersectedObjects = simulation.checkForObject(event);
         if (intersectedObjects.length > 0) {
-            for (let i = 0; i < simulation.items.length; i++) {
-                if (simulation.items[i].uuid == intersectedObjects[0].uuid) {
-                    objectNameField.innerText = simulation.items[i].name;
+            for (let i = 0; i < simulation.boxes.length; i++) {
+                if (simulation.boxes[i].mesh.uuid == intersectedObjects[0].object.uuid) {
+                    objectNameField.innerText = simulation.boxes[i].name;
                     itemSelected = i;
                     setInputObjectParameters();
-                    colorPicker.value = simulation.items[itemSelected].object.material.color;
-                    // colorPicker.style.initialValue = simulation.items[itemSelected].object.material.color;
+                    colorPicker.value = `#${simulation.boxes[i].mesh.material.color.getHexString()}`;
                     break;
                 } else {
                     itemSelected = -1;
@@ -154,8 +204,6 @@ canvas.addEventListener('mousedown', (event) => {
 }, false);
 
 window.addEventListener('resize', () => {
-    console.log("hello");
-    console.log(camera.aspect);
     camera.aspect = parseInt(window.getComputedStyle(topUI).width) / parseInt(window.getComputedStyle(rightUI).height);
     camera.updateProjectionMatrix();
 
@@ -164,8 +212,7 @@ window.addEventListener('resize', () => {
 });
 
 widthInput.addEventListener("blur", () => {
-    console.log(itemSelected);
-    if (widthInput.value.length == 0 || isNaN(widthInput.value) || itemSelected < 0) {
+    if ((widthInput.value.length == 0 || isNaN(widthInput.value)) && itemSelected > -1) {
         widthInput.focus();
     } else {
         simulation.items[itemSelected].dimensions.width = widthInput.value;
@@ -173,7 +220,7 @@ widthInput.addEventListener("blur", () => {
 });
 
 heightInput.addEventListener("blur", () => {
-    if (heightInput.value.length == 0 || isNaN(heightInput.value) || itemSelected < 0) {
+    if ((heightInput.value.length == 0 || isNaN(widthInput.value)) && itemSelected > -1) {
         heightInput.focus();
     } else {
         simulation.items[itemSelected].dimensions.height = heightInput.value;
@@ -181,7 +228,7 @@ heightInput.addEventListener("blur", () => {
 });
 
 depthInput.addEventListener("blur", () => {
-    if (depthInput.value.length == 0 || isNaN(depthInput.value) || itemSelected < 0) {
+    if ((depthInput.value.length == 0 || isNaN(widthInput.value)) && itemSelected > -1) {
         depthInput.focus();
     } else {
         simulation.items[itemSelected].dimensions.depth = depthInput.value;
@@ -189,7 +236,7 @@ depthInput.addEventListener("blur", () => {
 });
 
 xInput.addEventListener("blur", () => {
-    if (xInput.value.length == 0 || isNaN(xInput.value) || itemSelected < 0) {
+    if ((xInput.value.length == 0 || isNaN(widthInput.value)) && itemSelected > -1) {
         xInput.focus();
     } else {
         simulation.items[itemSelected].dimensions.x = xInput.value;
@@ -197,7 +244,7 @@ xInput.addEventListener("blur", () => {
 });
 
 yInput.addEventListener("blur", () => {
-    if (yInput.value.length == 0 || isNaN(yInput.value) || itemSelected < 0) {
+    if ((yInput.value.length == 0 || isNaN(widthInput.value)) && itemSelected > -1) {
         yInput.focus();
     } else {
         simulation.items[itemSelected].dimensions.y = yInput.value;
@@ -205,8 +252,7 @@ yInput.addEventListener("blur", () => {
 });
 
 zInput.addEventListener("blur", () => {
-    console.log(simulation.items[itemSelected]);
-    if (zInput.value.length == 0 || isNaN(zInput.value) || itemSelected < 0) {
+    if ((zInput.value.length == 0 || isNaN(widthInput.value)) && itemSelected > -1) {
         zInput.focus();
     } else {
         simulation.items[itemSelected].dimensions.z = zInput.value;
@@ -214,8 +260,7 @@ zInput.addEventListener("blur", () => {
 });
 
 colorPicker.addEventListener("change", (event) => {
-    console.log("hello2", itemSelected);
     if (itemSelected > -1) {
-        simulation.items[itemSelected].object.material.color.set(`${event.target.value}`);
+        simulation.boxes[itemSelected].mesh.material.color.set(`${event.target.value}`);
     }
-})
+});
