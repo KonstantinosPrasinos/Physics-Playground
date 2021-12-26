9b4fa6973c67295c782f276f22dc574b9506f748
KonstantinosPrasinos <konstantinos.prasinos@gmail.com>
2021-12-27 00:05:25 +0200

    Add button for uploading json Style upload button Add svg for upload

--- styling.js
+++ styling.js
@@ -1034,9 +1034,9 @@ document.getElementById("json-input").onchange = async function () {
 
 async function fileToJSON(file) {
     return new Promise((resolve, reject) => {
-      const fileReader = new FileReader()
-      fileReader.onload = event => resolve(JSON.parse(event.target.result))
-      fileReader.onerror = error => reject(error)
-      fileReader.readAsText(file)
+        const fileReader = new FileReader()
+        fileReader.onload = event => resolve(JSON.parse(event.target.result))
+        fileReader.onerror = error => reject(error)
+        fileReader.readAsText(file)
     })
-  }
+}
