1ec08d93f15a982ae2a1af9a1e53a73393d7d9c6
Konstantinos Prasinos <konstantinos.prasinos@gmail.com>
2023-09-08 17:25:53 +0300

    style: make globalStyles.css feat: add appearance section to settings feat: change how rounded corners work for canvas

--- scripts/tempStyling.js
+++ scripts/tempStyling.js
@@ -2,38 +2,56 @@ const rightUiToggle = document.getElementById("collapse-right-ui-button")
 const rightUi = document.getElementById("right-ui")
 const canvas = document.getElementById("viewportCanvas")
 const settingsUiToggle = document.getElementById("settings-button")
-const settingsUi = document.getElementById("settings-overlay")
+const settingsUi = document.getElementById("settings-box")
 const timeScaleSlider = document.getElementById("time-scale-slider");
-const timeScaleValue = document.getElementById("time-scale-slider-value")
+const timeScaleValue = document.getElementById("time-scale-slider-value");
+const closeSettingsButton = document.getElementById("close-settings-button");
+const leftRoundedCorner = document.getElementById("rounded-corner-left");
+const rightRoundedCorner = document.getElementById("rounded-corner-right")
 
 rightUiToggle.onclick = () => {
     if (rightUi.classList.contains("collapsed")) {
         rightUi.classList.remove("collapsed")
         rightUiToggle.classList.remove("collapsed")
-        canvas.classList.remove("collapsed")
+        rightRoundedCorner.classList.remove("collapsed")
     } else {
         rightUi.classList.add("collapsed")
         rightUiToggle.classList.add("collapsed")
-        canvas.classList.add("collapsed")
+        rightRoundedCorner.classList.add("collapsed")
     }
+}
+
+const collapseSettings = () => {
+    settingsUi.classList.add("collapsed")
+    leftRoundedCorner.classList.add("collapsed");
+    document.onclick = undefined;
+    settingsUi.ontransitionend = undefined;
 }
 
 settingsUiToggle.onclick = () => {
     if (settingsUi.classList.contains("collapsed")) {
-        settingsUi.classList.remove("collapsed")
+        settingsUi.classList.remove("collapsed");
+        leftRoundedCorner.classList.remove("collapsed");
+
+        // settingsUi.ontransitionend = () => {
+        //     document.onclick = (event) => {
+        //         if (event.target !== settingsUi) {
+        //             collapseSettings();
+        //         }
+        //     }
+        // }
     } else {
-        settingsUi.classList.add("collapsed")
+        collapseSettings();
     }
 }
 
-settingsUi.onclick = (event) => {
-    if (event.target === settingsUi) {
-        settingsUi.classList.add("collapsed")
-    }
+closeSettingsButton.onclick = () => {
+    collapseSettings();
 }
 
 timeScaleSlider.addEventListener("input", (event) => {
     const percentage = ((event.target.value - 0.25) / 2.75) * 100
+
     timeScaleValue.innerText = `${event.target.value} x`
     timeScaleValue.style.left = `calc(${percentage}% - ${percentage / 100 * 3.5}em)`
 });
