3e807badc1b4fe75b2656561e7d0d7a6883dee17
Konstantinos Prasinos <konstantinos.prasinos@gmail.com>
2023-09-11 00:00:37 +0300

    feat: make themes functional feat: add timeline ui feat: add all settings

--- scripts/tempStyling.js
+++ scripts/tempStyling.js
@@ -8,6 +8,14 @@ const timeScaleValue = document.getElementById("time-scale-slider-value");
 const closeSettingsButton = document.getElementById("close-settings-button");
 const leftRoundedCorner = document.getElementById("rounded-corner-left");
 const rightRoundedCorner = document.getElementById("rounded-corner-right")
+const lightThemeRadio = document.getElementById("light-theme-radio");
+const darkThemeRadio = document.getElementById("dark-theme-radio");
+const midnightThemeRadio = document.getElementById("midnight-theme-radio");
+const cameraFovSlider = document.getElementById("camera-fov-slider");
+const cameraFovValue = document.getElementById("camera-fov-slider-value");
+const timelineUi = document.getElementById("timeline-box");
+const timelineUiToggle = document.getElementById("timeline-button");
+const closeTimelineButton = document.getElementById("close-timeline-button");
 
 rightUiToggle.onclick = () => {
     if (rightUi.classList.contains("collapsed")) {
@@ -23,23 +31,18 @@ rightUiToggle.onclick = () => {
 
 const collapseSettings = () => {
     settingsUi.classList.add("collapsed")
-    leftRoundedCorner.classList.add("collapsed");
+    leftRoundedCorner.classList.remove("extended");
     document.onclick = undefined;
     settingsUi.ontransitionend = undefined;
 }
 
 settingsUiToggle.onclick = () => {
+    if (!timelineUi.classList.contains("collapsed")) {
+        collapseTimeline();
+    }
     if (settingsUi.classList.contains("collapsed")) {
         settingsUi.classList.remove("collapsed");
-        leftRoundedCorner.classList.remove("collapsed");
-
-        // settingsUi.ontransitionend = () => {
-        //     document.onclick = (event) => {
-        //         if (event.target !== settingsUi) {
-        //             collapseSettings();
-        //         }
-        //     }
-        // }
+        leftRoundedCorner.classList.add("extended");
     } else {
         collapseSettings();
     }
@@ -49,9 +52,57 @@ closeSettingsButton.onclick = () => {
     collapseSettings();
 }
 
+const collapseTimeline = () => {
+    timelineUi.classList.add("collapsed");
+    leftRoundedCorner.classList.remove("double-extended");
+    document.onclick = undefined;
+    timelineUi.ontransitionend = undefined;
+}
+
+timelineUiToggle.onclick = () => {
+    if (!settingsUi.classList.contains("collapsed")) {
+        collapseSettings();
+    }
+    if (timelineUi.classList.contains("collapsed")) {
+        timelineUi.classList.remove("collapsed");
+        leftRoundedCorner.classList.add("double-extended");
+    } else {
+        collapseTimeline();
+    }
+}
+
+closeTimelineButton.onclick = () => {
+    collapseTimeline();
+}
+
 timeScaleSlider.addEventListener("input", (event) => {
     const percentage = ((event.target.value - 0.25) / 2.75) * 100
 
     timeScaleValue.innerText = `${event.target.value} x`
     timeScaleValue.style.left = `calc(${percentage}% - ${percentage / 100 * 3.5}em)`
+});
+
+lightThemeRadio.onchange = () => {
+    if (document.body.className !== "light-theme") {
+        document.body.className = "light-theme";
+    }
+}
+
+darkThemeRadio.onchange = () => {
+    if (document.body.className !== "dark-theme") {
+        document.body.className = "dark-theme";
+    }
+}
+
+midnightThemeRadio.onchange = () => {
+    if (document.body.className !== "midnight-theme") {
+        document.body.className = "midnight-theme";
+    }
+}
+
+cameraFovSlider.addEventListener("input", (event) => {
+    const percentage = ((event.target.value - 20) / 90) * 100
+
+    cameraFovValue.innerText = `${event.target.value}`
+    cameraFovValue.style.left = `calc(${percentage}% - ${percentage / 100 * 3.5}em)`
 });
